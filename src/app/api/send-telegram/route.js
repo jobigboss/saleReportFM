b6ea317e461a09febc13a93584ac9b9cf385e317
Joe Bigboss <josursit@gmail.com>
2025-05-23 16:29:43 +0700

    update

--- src/app/api/send-telegram/route.js
+++ src/app/api/send-telegram/route.js
@@ -3,53 +3,6 @@
 import { connectMongoDB } from "../../../../lib/mongodb";
 import User from "../../../../models/sale_Report_User";
 
-    // export async function POST(req) {
-    //   const body = await req.json();
-    //   console.log("📥 Telegram Request Body:", body);
-
-    //   const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-    //   const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-
-    //   console.log("🟡 Token loaded?", !!TELEGRAM_TOKEN);
-    //   console.log("🟡 Chat ID loaded?", TELEGRAM_CHAT_ID);
-
-    //   const message = `
-    // 📢 รายงานใหม่จาก LIFF
-
-    // 👤 LINE ID: ${body.user_LineID}
-    // 🏪 ร้าน: ${body.store_Name}
-    // 📦 ช่องทาง: ${body.store_Channel}
-    // 📍 จังหวัด: ${body.store_Province}
-    // 📍 เขต: ${body.store_Area2}
-    // 💼 บัญชี: ${body.store_Account}
-    // `.trim();
-
-    //   const telegramUrl = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
-
-    //   try {
-    //     const res = await fetch(telegramUrl, {
-    //       method: "POST",
-    //       headers: { "Content-Type": "application/json" },
-    //       body: JSON.stringify({
-    //         chat_id: TELEGRAM_CHAT_ID,
-    //         text: message,
-    //         parse_mode: "Markdown",
-    //       }),
-    //     });
-
-    //     const data = await res.json();
-    //     console.log("📬 Telegram Response:", data);
-
-    //     return new Response(JSON.stringify({ success: data.ok }), {
-    //       status: data.ok ? 200 : 500,
-    //     });
-    //   } catch (error) {
-    //     console.error("❌ Error sending to Telegram:", error);
-    //     return new Response(JSON.stringify({ success: false }), {
-    //       status: 500,
-    //     });
-    //   }
-    // }
 export async function POST(req) {
   const body = await req.json();
   console.log("📥 Telegram Request Body:", body);
@@ -74,7 +27,6 @@ export async function POST(req) {
 
 👤 ผู้ใช้งาน: ${userName} ${userLastname}
 📱 เบอร์โทร: ${userPhone}
-🆔 LINE ID: ${body.user_LineID}
 🏪 ร้าน: ${body.store_Name}
 📦 ช่องทาง: ${body.store_Channel}
 📍 จังหวัด: ${body.store_Province}

adb85e714074854cc8381ccfebc0e8e930f543c8
Joe Bigboss <josursit@gmail.com>
2025-05-25 01:48:14 +0700

    update

---
+++ src/app/utils/getCheerSummaryByChannel.js
@@ -0,0 +1,4 @@
+// src/utils/getCheerSummaryByChannel.js
+export async function getCheerSummaryByChannel() {
+  return "ยังไม่ได้เขียนโค้ด";
+}
---
+++ src/app/utils/getPerformanceSummary.js
@@ -0,0 +1,4 @@
+// /utils/getPerformanceSummary.js
+export async function getPerformanceSummary() {
+  return "ยังไม่ได้เขียนโค้ด";
+}

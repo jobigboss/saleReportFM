dcf53afba1b1494662d14008bc70cd90fe3c23b5
peerapong026 <peerapong026@gmail.com>
2025-05-27 15:58:11 +0700

    first commit

---
+++ src/app/check_booth/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react' ;
+import Container from '../components/Container';
+import Survey_booth from './components/Survey_booth';
+
+function New_booth() {
+  return (
+    <Container>
+        <Survey_booth/>
+    </Container>
+  )
+}
+
+export default New_booth
